d702c3213e040a91e09f527e9d9a83ff8409fa72
l3miage-ourzikj <Jugurta.Ourzik@etu.univ-grenoble-alpes.fr>
2024-03-13 22:05:16 +0100

    +exo1: exo1 with create mode overall done

---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo1/models/ClubSportif.java
@@ -0,0 +1,17 @@
+package fr.uga.l3miage.tp2.exo1.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+
+@Entity
+public class ClubSportif {
+    @Id
+    private Long id;
+
+    private String nom;
+    private  String description;
+    @ManyToMany
+    private ArrayList<Etudiant> etudiants;
+}
